0cca3e909fee882694467d7cf2a536bb34327936
Madness-D <dingbuowen@outlook.com>
2021-09-03 14:37:09 +0800

    238 优化空间复杂度 仅使用一个数组

--- 238.除自身以外数组的乘积.cpp
+++ 238.除自身以外数组的乘积.cpp
@@ -8,29 +8,48 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        //两个数组，分别存储前缀元素的积，后缀元素的积
-        //三次遍历，应该时间复杂度为O(3n)?
-        //空间复杂度也是O(n)
+        // //两个数组，分别存储前缀元素的积，后缀元素的积
+        // //三次遍历，应该时间复杂度为O(3n)?
+        // //空间复杂度也是O(n)
+        // int len=nums.size();
+        // vector<int> q(len);
+        // vector<int> h(len);
+        // vector<int> res(len);
+        // int temp=1;
+        // q[0]=1;
+        // for(int i=1;i<len;++i){
+        //     temp*=nums[i-1];
+        //     q[i]=temp;
+        // }
+        // h[len-1]=1;
+        // temp=1;
+        // for(int i=len-2;i>=0;--i){
+        //     temp*=nums[i+1];
+        //     h[i]=temp;
+        // }
+        // for(int i=0;i<len;i++){
+        //     res[i]=q[i]*h[i];
+        // }
+        // return res;
+
+        //上面用到了三个数组，优化成1个
         int len=nums.size();
-        vector<int> q(len);
-        vector<int> h(len);
         vector<int> res(len);
         int temp=1;
-        q[0]=1;
+        res[0]=1;
         for(int i=1;i<len;++i){
             temp*=nums[i-1];
-            q[i]=temp;
-        }
-        h[len-1]=1;
+            res[i]=temp;
+        }//此时res内存的前缀积
+        //再逆序遍历一次，乘上后缀积
         temp=1;
         for(int i=len-2;i>=0;--i){
             temp*=nums[i+1];
-            h[i]=temp;
-        }
-        for(int i=0;i<len;i++){
-            res[i]=q[i]*h[i];
+            res[i]*=temp;
         }
         return res;
+
+
 
     }
 };
