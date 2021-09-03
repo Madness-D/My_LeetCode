bc7e99db49b61768419824e7cc9af6a23b4d8ddf
Madness-D <dingbuowen@outlook.com>
2021-09-03 14:16:24 +0800

    334 ac

--- 334.递增的三元子序列.cpp
+++ 334.递增的三元子序列.cpp
@@ -16,7 +16,25 @@ public:
         // }
         // return false;
 
-        //
+        //nums = [2,1,5,0,4,6];  1,5,6和 0，4，6都符合条件
+        if(len<3) return false;
+        int small=INT_MAX,mid=INT_MAX;
+        for(int n:nums){
+            if(n<=small){//需要有=，例如1 1 6，不符合条件
+                small=n;
+                //只更新small,而不更新mid，为什么？
+                //考虑4，6，1，2，3
+                //s=4,m=6
+                //s=1,m=6//这个时候如果不更新small，不会发现后面的123这个符合条件的
+                // //而且更新small而不更新mid，暗含了mid前面有一个比mid小的数，这样如果1后面是一个7，也能return true
+                //s=1,m=2
+            }else if(n<=mid){
+                mid=n;
+            }else if(n>mid){
+                return true;
+            }
+        }
+        return false;
 
     }
 };
