4b8ee066a585efdd6828ffd92b85a1f6a259e234
Madness-D <dingbuowen@outlook.com>
2021-09-03 14:01:14 +0800

    334 gg

---
+++ 334.递增的三元子序列.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=334 lang=cpp
+ *
+ * [334] 递增的三元子序列
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int len=nums.size();
+        // //是子序列，不是子串，gg
+        // for(int i=0;i<len-2;i++){
+        //     if(nums[i]<nums[i+1]&&nums[i+1]<nums[i+2])
+        //         return true;
+        // }
+        // return false;
+
+        //
+
+    }
+};
+// @lc code=end
+
