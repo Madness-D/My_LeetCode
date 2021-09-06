79b85cd7a6e668ade1e63a62c9d53eae6c7ed3ad
Madness_D <dingbuowen@outlook.com>
2021-09-07 00:13:08 +0800

    0907每日一题 1221

---
+++ 5.最长回文子串.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=5 lang=cpp
+ *
+ * [5] 最长回文子串
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        //一个经典的问题
+        //动态规划，复杂度可以达到n^2
+        
+
+    }
+};
+// @lc code=end
+
