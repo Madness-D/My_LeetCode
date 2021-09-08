63ea1655a3f14bd4c39df88b5b18045cf21afefc
Madness_D <dingbuowen@outlook.com>
2021-09-08 17:19:04 +0800

    739 单调栈

--- 739.每日温度.cpp
+++ 739.每日温度.cpp
@@ -23,18 +23,34 @@ public:
         // }
         // //res.push_back(0);
         // return res;
+
+        //官方题解
+        // int n = temperatures.size();
+        // vector<int> ans(n), next(101, INT_MAX);//数组 next 记录每个温度第一次出现的下标
+        // //一定是反向遍历
+        // for (int i = n - 1; i >= 0; --i) {
+        //     int warmerIndex = INT_MAX;
+        //     for (int t = temperatures[i] + 1; t <= 100; ++t) {
+        //         warmerIndex = min(warmerIndex, next[t]);
+        //     }
+        //     if (warmerIndex != INT_MAX) {
+        //         ans[i] = warmerIndex - i;
+        //     }
+        //     next[temperatures[i]] = i;
+        // }
+        // return ans;
+
+        //单调栈
         int n = temperatures.size();
-        vector<int> ans(n), next(101, INT_MAX);//数组 next 记录每个温度第一次出现的下标
-        //一定是反向遍历
-        for (int i = n - 1; i >= 0; --i) {
-            int warmerIndex = INT_MAX;
-            for (int t = temperatures[i] + 1; t <= 100; ++t) {
-                warmerIndex = min(warmerIndex, next[t]);
+        vector<int> ans(n);
+        stack<int> s;
+        for (int i = 0; i < n; ++i) {
+            while (!s.empty() && temperatures[i] > temperatures[s.top()]) {
+                int previousIndex = s.top();
+                ans[previousIndex] = i - previousIndex;
+                s.pop();
             }
-            if (warmerIndex != INT_MAX) {
-                ans[i] = warmerIndex - i;
-            }
-            next[temperatures[i]] = i;
+            s.push(i);
         }
         return ans;
 
