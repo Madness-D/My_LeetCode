22c440cc1b18fcde61d6675698a07a70712d933c
Madness_D <dingbuowen@outlook.com>
2021-09-12 10:42:14 +0800

    1249  两次遍历

---
+++ 1823.找出游戏的获胜者.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1823 lang=cpp
+ *
+ * [1823] 找出游戏的获胜者
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        
+    }
+};
+// @lc code=end
+
