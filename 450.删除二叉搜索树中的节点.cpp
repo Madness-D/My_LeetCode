8e61601d15dceecf72f23b70716df661065ab335
Madness-D <dingbuowen@outlook.com>
2021-09-14 15:10:56 +0800

    450 题解

--- 450.删除二叉搜索树中的节点.cpp
+++ 450.删除二叉搜索树中的节点.cpp
@@ -31,6 +31,8 @@ public:
         }
         return root;
     }
+
+
     void mydeleteNode(TreeNode* &root,int x){//删除值为x的结点
     if(root==nullptr) return;
     if(root->val ==x){//找到结点
@@ -54,9 +56,25 @@ public:
 }
 
     TreeNode* deleteNode(TreeNode* root, int key) {
-        //写一个函数，查找，返回
-        mydeleteNode(root,key);
-        return root;
+        
+        // mydeleteNode(root,key);
+        // return root;
+
+        //https://leetcode-cn.com/problems/delete-node-in-a-bst/solution/miao-dong-jiu-wan-shi-liao-by-terry2020-tc0o/
+    if (root == nullptr)    return nullptr;
+        if (key > root->val)    root->right = deleteNode(root->right, key);     // 去右子树删除
+        else if (key < root->val)    root->left = deleteNode(root->left, key);  // 去左子树删除
+        else    // 当前节点就是要删除的节点
+        {
+            if (! root->left)   return root->right; // 情况1，欲删除节点无左子
+            if (! root->right)  return root->left;  // 情况2，欲删除节点无右子
+            TreeNode* node = root->right;           // 情况3，欲删除节点左右子都有 
+            while (node->left)          // 寻找欲删除节点右子树的最左节点
+                node = node->left;
+            node->left = root->left;    // 将欲删除节点的左子树成为其右子树的最左节点的左子树
+            root = root->right;         // 欲删除节点的右子顶替其位置，节点被删除
+        }
+        return root;    
     }
 };
 // @lc code=end
