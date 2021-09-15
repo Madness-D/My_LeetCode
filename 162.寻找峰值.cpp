e327e1314a34b5f0de3f8517b0f349f418c841ee
Madness_D <dingbuowen@outlook.com>
2021-09-15 10:15:47 +0800

    162 每日一题 遍历

---
+++ 162.寻找峰值.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=162 lang=cpp
+ *
+ * [162] 寻找峰值
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        //遍历
+        if(nums.size()==2){
+            return nums[0]>nums[1]?0:1;
+        }
+        nums.push_back(-INT_MAX);//防止出现最后一个为答案的情况
+        for(int i=1;i<nums.size()-1;i++){
+            if(nums[i]>nums[i-1]&&nums[i]>nums[i+1]){
+                return i;
+            }
+        }
+        return 0;
+    }
+};
+// @lc code=end
+
