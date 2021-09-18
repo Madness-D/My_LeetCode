a697f8157f7e9c487bce3bb233357ad3977c9cbd
Madness_D <dingbuowen@outlook.com>
2021-09-19 00:00:31 +0800

    451 堆 使用优先队列

--- 451.根据字符出现频率排序.cpp
+++ 451.根据字符出现频率排序.cpp
@@ -7,48 +7,51 @@
 // @lc code=start
 class Solution {
 public:
-    // string frequencySort(string s) {
-    //     //哈希表
-    //     unordered_map<char, int> mp;
-    //     for (auto i : s) mp[i]++;
-
-    //     priority_queue<pair<char, int>> pq;
-    //     for (auto p : mp) {//将哈希表中的key和value组成pair存到队列中
-    //         //
-    //         pq.push(pair<char, int>(p.second, p.first));
-    //     }
-
-    //     string res;
-    //     //
-    //     while(!pq.empty()){
-    //         int k=pq.top().second;
-    //         while(k--){
-    //             res+=pq.top().first;
-    //         }
-    //         pq.pop();
-    //     }
-
-    //     return res;
-
-    // }
-    using PIC = pair<int, char>;
     string frequencySort(string s) {
-        unordered_map<char, int> um;
-        for(auto c:s)
-            um[c]++;
-        
-        priority_queue<PIC, vector<PIC>, less<PIC>> pq;
-        for(auto &[key, value] : um)
+        //哈希表
+        unordered_map<char, int> mp;
+        for (auto i : s) mp[i]++;
+
+        priority_queue<pair<int,char>> pq;
+        for (auto &[key, value] : mp) {//将哈希表中的key和value组成pair存到队列中
+            //
             pq.push({value, key});
-        
-        string ans = "";
-        while(!pq.empty()) {
-            auto [num, c] = pq.top();
-            pq.pop();
-            ans += string(num, c);
         }
-        return ans;
+
+        string res;
+        //
+        while(!pq.empty()){
+            auto [num, c] = pq.top();
+            // while(num--){
+            //     res+=c;
+            // }
+            res+=string(num,c);
+            pq.pop();
+        }
+
+        return res;
+
     }
+
+    // //复制粘贴
+    // using PIC = pair<int, char>;
+    // string frequencySort(string s) {
+    //     unordered_map<char, int> um;
+    //     for(auto c:s)
+    //         um[c]++;
+        
+    //     priority_queue<PIC, vector<PIC>, less<PIC>> pq;
+    //     for(auto &[key, value] : um)
+    //         pq.push({value, key});
+        
+    //     string ans = "";
+    //     while(!pq.empty()) {
+    //         auto [num, c] = pq.top();
+    //         pq.pop();
+    //         ans += string(num, c);
+    //     }
+    //     return ans;
+    // }
 
 
 };
