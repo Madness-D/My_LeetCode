f1b86c8f05aad91ae4bcc7056510e39d259bd35f
Madness_D <dingbuowen@outlook.com>
2021-09-19 09:56:57 +0800

    973 优先队列

---
+++ 650.只有两个键的键盘.cpp
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=650 lang=cpp
+ *
+ * [650] 只有两个键的键盘
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int minSteps(int n) {
+        //两种操作
+
+    }
+};
+// @lc code=end
+
