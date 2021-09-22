e9ebaf85473dec236a9497426df57176f65cdab4
Madness_D <dingbuowen@outlook.com>
2021-09-23 00:10:33 +0800

    326 数论方法

--- 326.3-的幂.cpp
+++ 326.3-的幂.cpp
@@ -14,6 +14,9 @@ public:
         if(n==3||n==1) return true;
         else return isPowerOfThree(n/3);
 
+        // //在题目给定的 3232 位有符号整数的范围内，最大的3 的幂为 3^{19} = 11622614673 我们只需要判断 n 是否是 3^{19}的约数即可
+        // return n > 0 && 1162261467 % n == 0;
+
     }
 };
 // @lc code=end
