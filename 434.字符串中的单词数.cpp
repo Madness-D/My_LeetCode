a85e7a01d9f6d767ecdfc7f4d108f70320231076
Madness_D <dingbuowen@outlook.com>
2021-10-07 00:15:07 +0800

    434 划分单词

--- 434.字符串中的单词数.cpp
+++ 434.字符串中的单词数.cpp
@@ -8,14 +8,33 @@
 class Solution {
 public:
     int countSegments(string s) {
-        //根据空格或者是起始位置，来计数
-        int res=0;
+        // //根据空格或者是起始位置，来计数
+        // int res=0;
+        // for(int i=0;i<s.size();i++){
+        //     if((i==0||s[i-1]==' ' )&& s[i]!=' '){
+        //         res++;
+        //     }
+        // }
+        // return res;
+
+        //划分单词
+        vector<string> v;
+        string str;
         for(int i=0;i<s.size();i++){
-            if((i==0||s[i-1]==' ' )&& s[i]!=' '){
-                res++;
+            if(s[i]!=' '){
+                str.push_back(s[i]);
+            }else if(s[i]==' '){
+                if(!str.empty()){
+                    v.push_back(str);
+                }
+                str.clear();
             }
         }
-        return res;
+        if(!str.empty()){
+                    v.push_back(str);
+        }
+
+        return v.size();
 
     }
 };
