f4e3a8bc1624297500e530d68afd08bf314026ef
Madness_D <dingbuowen@outlook.com>
2021-10-08 09:39:20 +0800

    16 暴力法

---
+++ 16.最接近的三数之和.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=16 lang=cpp
+ *
+ * [16] 最接近的三数之和
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        int len=nums.size();
+        if(len<3) return {};
+
+        //暴力法
+        int res=nums[0]+nums[1]+nums[2];
+        for(int i=0;i<len-2;i++){
+            for(int j=i+1;j<len-1;j++){
+                for(int k=j+1;k<len;k++){
+                    int temp=nums[i]+nums[j]+nums[k];
+                    if(abs(temp-target)<abs(res-target)){
+                        res=temp;
+                    }
+                }
+            }
+        }
+        return res;
+        
+    }
+};
+// @lc code=end
+
