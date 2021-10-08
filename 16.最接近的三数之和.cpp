594a0b151bb41c7f1507f738217a6eb4e665e663
Madness_D <dingbuowen@outlook.com>
2021-10-08 10:08:44 +0800

    16 双指针

--- 16.最接近的三数之和.cpp
+++ 16.最接近的三数之和.cpp
@@ -11,20 +11,42 @@ public:
         int len=nums.size();
         if(len<3) return {};
 
-        //暴力法
+        // //暴力法
+        // int res=nums[0]+nums[1]+nums[2];
+        // for(int i=0;i<len-2;i++){
+        //     for(int j=i+1;j<len-1;j++){
+        //         for(int k=j+1;k<len;k++){
+        //             int temp=nums[i]+nums[j]+nums[k];
+        //             if(abs(temp-target)<abs(res-target)){
+        //                 res=temp;
+        //             }
+        //         }
+        //     }
+        // }
+        // return res;
+
+        //排序后双指针
+        sort(nums.begin(),nums.end());
         int res=nums[0]+nums[1]+nums[2];
         for(int i=0;i<len-2;i++){
-            for(int j=i+1;j<len-1;j++){
-                for(int k=j+1;k<len;k++){
-                    int temp=nums[i]+nums[j]+nums[k];
-                    if(abs(temp-target)<abs(res-target)){
-                        res=temp;
-                    }
+            int l=i+1,r=len-1;
+            while(l<r){
+                int temp=nums[i]+nums[l]+nums[r];
+                //更新res
+                if(temp==target){
+                    return temp;
+                }else if(abs(temp-target)<abs(res-target)){
+                    res=temp;
+                }
+                //移动指针
+                if(temp<target){
+                    l++;
+                }else{
+                    r--;
                 }
             }
         }
         return res;
-        
     }
 };
 // @lc code=end
