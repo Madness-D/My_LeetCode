7e008ffcfb81e9135ff1f519e2a62cf8d52678fb
Madness_D <dingbuowen@outlook.com>
2021-10-20 23:03:13 +0800

    148 数组 排序 新建链表

--- 148.排序链表.cpp
+++ 148.排序链表.cpp
@@ -17,9 +17,48 @@
  */
 class Solution {
 public:
+    ListNode* creatList(vector<int> a){
+        int n=a.size();
+        ListNode* root=new ListNode(a[0]);
+        ListNode* pnode=root;
+
+        for(int i=0;i<n-1;i++){
+            root->val=a[i];
+            root->next=nullptr;
+
+            ListNode *p= new ListNode();
+            p->val=a[i+1];
+            p->next=nullptr;
+
+            root->next=p;
+            //root=p;
+            root=root->next;
+        }
+
+        root->next=nullptr;
+        return pnode;
+    }
+
     ListNode* sortList(ListNode* head) {
+        if(!head) return head;
+        //先来一个暴力的方法，将链表里的数据存到数组里，sort，然后再构建一个链表
         vector<int> nums;
-    
+        ListNode* curr=head;
+        while(curr){
+            nums.push_back(curr->val);
+            curr=curr->next;
+        }
+        //排序
+        sort(nums.begin(),nums.end());
+        ////构建新的链表
+        // ListNode* res;
+        // curr=res;
+        // for(int i=0;i<nums.size();i++){
+        //     curr->val=nums[i];
+        //     curr=curr->next;
+        // }
+        
+        return creatList(nums);
     }
 };
 // @lc code=end
