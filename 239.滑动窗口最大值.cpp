999b7a72c283eddb1bf51db786cad67c25f80e11
Madness_D <dingbuowen@outlook.com>
2021-10-21 19:55:07 +0800

    239  单调队列

--- 239.滑动窗口最大值.cpp
+++ 239.滑动窗口最大值.cpp
@@ -32,24 +32,47 @@ public:
         // }
         // return res;
 
-        //优先队列
+        // //优先队列
+        // int n = nums.size();
+        // //第一个int存数值，第二个int存下标
+        // priority_queue<pair<int, int>> q;
+        // //将前k个元素存入队列
+        // for (int i = 0; i < k; ++i) {
+        //     q.emplace(nums[i], i);
+        // }
+        // //vector<int> ans = {q.top().first};
+        // vector<int> ans;
+        // ans.push_back(q.top().first);
+        // for (int i = k; i < n; ++i) {
+        //     q.emplace(nums[i], i);
+        //     //只有在最大元素的下标不在窗口内的时候才需要pop，否则其实没有影响
+        //     while (q.top().second <= i - k) {
+        //         q.pop();
+        //     }
+        //     ans.push_back(q.top().first);
+        // }
+        // return ans;
+
+        //单调、双端队列
         int n = nums.size();
-        //第一个int存数值，第二个int存下标
-        priority_queue<pair<int, int>> q;
-        //将前k个元素存入队列
+        deque<int> q;
         for (int i = 0; i < k; ++i) {
-            q.emplace(nums[i], i);
-        }
-        //vector<int> ans = {q.top().first};
-        vector<int> ans;
-        ans.push_back(q.top().first);
-        for (int i = k; i < n; ++i) {
-            q.emplace(nums[i], i);
-            //只有在最大元素的下标不在窗口内的时候才需要pop，否则其实没有影响
-            while (q.top().second <= i - k) {
-                q.pop();
+            while (!q.empty() && nums[i] >= nums[q.back()]) {
+                q.pop_back();
             }
-            ans.push_back(q.top().first);
+            q.push_back(i);
+        }
+
+        vector<int> ans = {nums[q.front()]};
+        for (int i = k; i < n; ++i) {
+            while (!q.empty() && nums[i] >= nums[q.back()]) {
+                q.pop_back();
+            }
+            q.push_back(i);
+            while (q.front() <= i - k) {
+                q.pop_front();
+            }
+            ans.push_back(nums[q.front()]);
         }
         return ans;
 
