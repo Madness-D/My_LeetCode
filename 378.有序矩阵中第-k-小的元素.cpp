1d8b056307babee7633cd020f38ed6a9f48794ae
Madness_D <dingbuowen@outlook.com>
2021-10-22 10:59:08 +0800

    378 暴力

---
+++ 378.有序矩阵中第-k-小的元素.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=378 lang=cpp
+ *
+ * [378] 有序矩阵中第 K 小的元素
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        // //来一个暴力的方法
+        // vector<int> nums;
+        // for(int i=0;i<matrix.size();i++){
+        //     for(int j=0;j<matrix[0].size();j++)
+        //     nums.push_back(matrix[i][j]);
+        // }
+        // sort(nums.begin(),nums.end());
+        // return nums[k-1];
+
+    }
+};
+// @lc code=end
+
