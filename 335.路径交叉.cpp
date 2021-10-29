efbf1fc850dfa502154daebc9f911633e2f5a5b2
Madness_D <dingbuowen@outlook.com>
2021-10-29 22:06:35 +0800

    335 复制粘贴

--- 335.路径交叉.cpp
+++ 335.路径交叉.cpp
@@ -10,14 +10,29 @@ public:
     bool isSelfCrossing(vector<int>& distance) {
         // //也没说只走一圈啊,想啥捏
         // return (distance[0]>=distance[2])&&(distance[1]<=distance[3]);
-        int  up=0,down=0,left=0,right=0;
-        for(int i=0;i<distance.size();i++){
-            if(i%4==0) up+=distance[i];
-            else if(i%4==1) left+=distance[i];
-            else if(i%4==2) down+=distance[i];
-            else if(i%4==3) right+=distance[i];
+
+int n = distance.size();
+        for (int i = 3; i < n; ++i) {
+            // 第 1 类路径交叉的情况
+            if (distance[i] >= distance[i - 2] && distance[i - 1] <= distance[i - 3]) {
+                return true;
+            }
+
+            // 第 2 类路径交叉的情况
+            if (i == 4 && (distance[3] == distance[1]
+                && distance[4] >= distance[2] - distance[0])) {
+                return true;
+            }
+
+            // 第 3 类路径交叉的情况
+            if (i >= 5 && (distance[i - 3] - distance[i - 5] <= distance[i - 1]
+                && distance[i - 1] <= distance[i - 3]
+                && distance[i] >= distance[i - 2] - distance[i - 4]
+                && distance[i - 2] > distance[i - 4])) {
+                return true;
+            }
         }
-        return (up>=down && left<=right);
+        return false;
     }
 };
 // @lc code=end
