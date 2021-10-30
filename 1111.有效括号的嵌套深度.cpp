1fb55b1713535e163e918d5980af5096ff245467
Madness_D <dingbuowen@outlook.com>
2021-10-30 20:05:51 +0800

    1111 栈

--- 1111.有效括号的嵌套深度.cpp
+++ 1111.有效括号的嵌套深度.cpp
@@ -9,15 +9,17 @@ class Solution {
 public:
     vector<int> maxDepthAfterSplit(string seq) {
         //对于在原串中的层数,将奇数层的分给A,偶数层的分给B
+        //对于层数,如果遇到(,则层数++,再分配
+        //遇到), 先分配, 再层数--
         int d=0;//层数
         vector<int> res;
         for(char ch:seq){
             if(ch=='('){
-                d++;
+                ++d;
                 res.push_back(d%2);
             }else{
                 res.push_back(d%2);
-                d--;
+                --d;
             }
         }
         return res;
