64e4f3d92aa490d6c5dac31c6b70a97cd4e4a1ff
Madness_D <dingbuowen@outlook.com>
2021-11-04 09:06:00 +0800

    367 暴力法

--- 367.有效的完全平方数.cpp
+++ 367.有效的完全平方数.cpp
@@ -11,21 +11,31 @@ public:
         //调用库函数捏
         //return num==((int)sqrt(num)*(int)sqrt(num));
 
-        if(num==1) return true;
-        int left=1,right=num;
-        int mid=left+(right-left)/2;
-        while(left<=right){//这里应该是=号
-            if((long)mid*mid==num){
+        // if(num==1) return true;
+        // int left=1,right=num;
+        // int mid=left+(right-left)/2;
+        // while(left<=right){//这里应该是=号
+        //     if((long)mid*mid==num){
+        //         return true;
+        //     }
+        //     else if((long)mid*mid<num){
+        //         left=mid+1;
+        //         mid=left+(right-left)/2;
+        //     }
+        //     else{
+        //         right=mid-1;
+        //         mid=left+(right-left)/2;
+        //     }
+        // }
+        // return false;
+
+        //暴力
+        long x=1,squa=1;
+        while(squa<=num){
+            squa=x*x;
+            if(squa==num)
                 return true;
-            }
-            else if((long)mid*mid<num){
-                left=mid+1;
-                mid=left+(right-left)/2;
-            }
-            else{
-                right=mid-1;
-                mid=left+(right-left)/2;
-            }
+            x++;
         }
         return false;
     }
