7755d10a3c4116f86c1088426c624138863ec0e4
Madness_D <dingbuowen@outlook.com>
2021-11-07 00:16:58 +0800

    268 598 easy

--- 268.丢失的数字.cpp
+++ 268.丢失的数字.cpp
@@ -8,15 +8,26 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int l=0,r=nums.size();//这里的r不能是n-1  ??
-        int mid;
-        while(l<r){
-            mid=(l+r)/2;
-            if(nums[mid]>mid) r=mid;
-            else l=mid+1;
-        }
-    return l;
+    //     sort(nums.begin(),nums.end());
+    //     int l=0,r=nums.size();//这里的r不能是n-1  ??
+    //     int mid;
+    //     while(l<r){
+    //         mid=(l+r)/2;
+    //         if(nums[mid]>mid) r=mid;
+    //         else l=mid+1;
+    //     }
+    // return l;
+
+    //数学
+    int n=nums.size();
+    long sum=n*(n+1)/2;
+    long rsum=0;
+    for(int num:nums){
+        rsum+=num;
+    }
+    return sum-rsum;
+
+    
     }
 };
 // @lc code=end
