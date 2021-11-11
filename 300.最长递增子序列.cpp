58e75aa95bd3ebc2cd5dc10114b5af8116d0f702
Madness_D <dingbuowen@outlook.com>
2021-11-11 20:40:19 +0800

    46 复制粘贴

--- 300.最长递增子序列.cpp
+++ 300.最长递增子序列.cpp
@@ -8,6 +8,21 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
+        // //看错题了,下面的是求最长递增子串
+        // int l=0,r=0;
+        // int res=0;
+        // while(r<nums.size()-1){
+        //     if(nums[r]<nums[r+1]){
+        //         r++;
+        //     }else{
+        //         res=res>(r-l+1)?res:(r-l+1);
+        //         r++;
+        //         l=r;
+        //     }
+        // }
+        // return res;
+
+        
 
     }
 };
