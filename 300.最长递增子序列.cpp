23db20214703f13b887cfb7e74a1b845cebfa10e
Madness_D <dingbuowen@outlook.com>
2021-11-13 23:49:46 +0800

    46复制粘贴

--- 300.最长递增子序列.cpp
+++ 300.最长递增子序列.cpp
@@ -22,7 +22,29 @@ public:
         // }
         // return res;
 
-        
+        // //来一个O(n^2)的
+        // //但是 [0,1,0,3,2,3] 会识别013,输出3
+        // int res=0;
+        // for(int i=0;i<nums.size();i++){
+        //     int temp=1; 
+        //     int pre=nums[i];
+        //     for(int j=i;j<nums.size();j++){
+        //         if(nums[j]>pre){
+        //             pre=nums[j];
+        //             temp++;
+        //         }
+        //     }
+        //     res=res>temp?res:temp;
+        // }
+        // return res;
+
+    vector<int> res;
+    for(int i=0; i<nums.size(); i++) {
+        auto it = std::lower_bound(res.begin(), res.end(), nums[i]);
+        if(it==res.end()) res.push_back(nums[i]);
+        else *it = nums[i];
+    }
+    return res.size();
 
     }
 };
