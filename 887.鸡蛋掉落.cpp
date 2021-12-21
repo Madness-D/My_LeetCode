b53ccf21f16fb7f25b9f0dc888a092f32f8aa6be
Madness_D <dingbuowen@outlook.com>
2021-12-21 22:33:55 +0800

    复制粘贴

---
+++ 887.鸡蛋掉落.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=887 lang=cpp
+ *
+ * [887] 鸡蛋掉落
+ */
+
+// @lc code=start
+class Solution {
+public:
+int calcF(int K, int T)
+    {
+        if (T == 1 || K == 1) return T + 1;
+        return calcF(K - 1, T - 1) + calcF(K, T - 1);
+    }
+
+    int superEggDrop(int K, int N)
+    {
+        int T = 1;
+        while (calcF(K, T) < N + 1) T++;
+        return T;
+    }
+
+};
+// @lc code=end
+
